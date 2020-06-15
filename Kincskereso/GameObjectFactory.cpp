82a758ca2413c9788e715c04c2dc05f44a9ac699
Arvai Peter <arvaipeter1@gmail.com>
2020-06-15 21:12:08 +0200

    Added factory class for game objects

---
+++ Kincskereso/GameObjectFactory.cpp
@@ -0,0 +1,34 @@
+#include "GameObjectFactory.h"
+
+
+const GameObjectFactory& GameObjectFactory::GetInstance() {
+	static GameObjectFactory instance;
+	return instance;
+}
+
+GameObject GameObjectFactory::CreateGameObject(const unsigned int& x, const unsigned int& y, const char& repr) const{
+	switch (repr)
+	{
+	case 'k':
+		return Consumable(x, y, ConsumableType::TREASURE);
+	case 'i':
+		return Consumable(x, y, ConsumableType::POTION);
+	case 'c':
+		return Environment(x, y, EnvironmentType::TRAP);
+	case 'a':
+		return Consumable(x, y, ConsumableType::SWORD);
+	case 'h':
+		return Player(x, y);
+	case 'x':
+		return Environment(x, y, EnvironmentType::WALL);
+	case 'j':
+		return Environment(x, y, EnvironmentType::EXIT);
+	case 's':
+		return Beast(x, y);
+	case ' ':
+		return Environment(x, y, EnvironmentType::PATH);
+	default:
+		// TODO: error unrecognised game object
+		break;
+	}
+}
---
+++ Kincskereso/GameObjectFactory.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameObjects.h"
+
+
+
+class GameObjectFactory
+{
+public:
+	static const GameObjectFactory& GetInstance();
+
+	GameObject CreateGameObject(const unsigned int& x, const unsigned int& y, const char& repr) const;
+
+	GameObjectFactory(const GameObjectFactory& ohter) = delete;
+	GameObjectFactory& operator=(const GameObjectFactory& other) = delete;
+
+private:
+	GameObjectFactory() = default;
+};
