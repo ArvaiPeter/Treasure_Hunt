2a7df6910d1834609ee8cfa13c53df92035d2809
Arvai Peter <arvaipeter1@gmail.com>
2020-06-15 21:18:06 +0200

    Updated main with temprorary tests for GameModel and ConsoleView

--- Kincskereso/TreasureHunt.cpp
+++ Kincskereso/TreasureHunt.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
 
 #include "ConsoleView.h"
+#include "TreasureHuntGameModel.h"
 
 int main() {
 
-	HANDLE consoleHandle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(consoleHandle);
+	const unsigned int windowWidth = 80;
+	const unsigned int windowHeight = 50;
 
-	ConsoleView testView(10, 10, &consoleHandle);
+	TreasureHuntGameModel model;
+	model.LoadLevel(L"level_1.txt");
 
+	//maybe move this to ConsoleView
+	
+
+	auto levelDimensions = model.GetLevelDimensions();
+	ConsoleView testView(windowWidth,windowHeight);
+
+	//TODO move to contoller
+	auto& lvl = model.GetLevel();
+	wchar_t* newScreenBuff = new wchar_t[windowWidth * windowHeight];
+
+	size_t elementCount = 0;
+	for (auto x = 0; x < windowHeight; ++x) {
+		for (auto y = 0; y < windowWidth; ++y) {
+			auto index = y + windowWidth * x;
+			wchar_t c = L'#';
+
+			if (y >= levelDimensions.first || x >= levelDimensions.second) {
+				c = L' ';
+			}
+			else {
+				c = lvl[elementCount++].GetRepresentation();
+			}
+
+			newScreenBuff[index] = c;
+		}
+	}
+
+	testView.SetScreenBuffer(newScreenBuff);
 	testView.DrawFrame();
 
 	// remove when game loop is ready
