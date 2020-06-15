feb56be24014b1f0184626dea28133ca82ab3d71
Arvai Peter <arvaipeter1@gmail.com>
2020-06-15 21:16:30 +0200

    Added game model

--- Kincskereso/TreasureHuntGameModel.cpp
+++ Kincskereso/TreasureHuntGameModel.cpp
@@ -1,1 +1,43 @@
 #include "TreasureHuntGameModel.h"
+
+
+TreasureHuntGameModel::TreasureHuntGameModel() 
+	: m_GameObjectFactory(GameObjectFactory::GetInstance()) {}
+
+void TreasureHuntGameModel::LoadLevel(const std::wstring& lvlName) {
+	// TODO: error handling
+
+	m_LevelName = lvlName;
+	//finding file
+	const std::wstring fullLvlPath = m_AssetFolderPath + L"\\" + m_LevelName;
+
+	//reading file and populating m_Level;
+	std::ifstream lvlFile(fullLvlPath.c_str(), std::ifstream::in);
+
+	std::string line;
+
+	unsigned int y = 0;
+	while (std::getline(lvlFile, line)) {
+		if (m_lvlWidth == 0) {
+			m_lvlWidth = line.length();
+		}
+
+		unsigned int x = 0;
+		for (const auto& lvlElement : line) {
+			auto element = m_GameObjectFactory.CreateGameObject(x, y, lvlElement);
+			m_Level.push_back(element);
+			++x;
+		}
+
+		++y;
+	}
+	m_lvlHeight = y;
+}
+
+const std::vector<GameObject>& TreasureHuntGameModel::GetLevel() const {
+	return m_Level;
+}
+
+std::pair<unsigned int, unsigned int> TreasureHuntGameModel::GetLevelDimensions() const {
+	return { m_lvlWidth, m_lvlHeight };
+}
--- Kincskereso/TreasureHuntGameModel.h
+++ Kincskereso/TreasureHuntGameModel.h
@@ -1,9 +1,32 @@
 #pragma once
-#include "IGameModel.h"
 
-class TreasureHuntGameModel :
-    public IGameModel
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "GameObjectFactory.h"
+
+class TreasureHuntGameModel
 {
+public:
+	TreasureHuntGameModel();
+
+	void LoadLevel(const std::wstring& lvlName);
+
+	const std::vector<GameObject>& GetLevel() const;
+
+	std::pair<unsigned int /*width*/, unsigned int /*heigth*/> GetLevelDimensions() const;
+
+private:
+	std::wstring m_LevelName;
+	std::vector<GameObject> m_Level;
+
+	// TODO: experimantal path or custom path handling
+	const std::wstring m_AssetFolderPath = L"..\\Assets";
+
+	const GameObjectFactory& m_GameObjectFactory;
+	
+	unsigned int m_lvlWidth;
+	unsigned int m_lvlHeight;
 
 };
-
